9f9492f32ae53b983840aadb519cf302dcf361ed
yasmineMei <meiteyasmine90@gmail.com>
2025-02-24 21:42:03 +0100

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
