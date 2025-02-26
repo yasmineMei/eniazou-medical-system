b65560e4218adb4df9a134d3d8f90ea448c586a9
yasmineMei <meiteyasmine90@gmail.com>
2025-02-26 12:46:25 +0100

    add devtools tanstack-router

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,10 +5,25 @@ export const Route = createRootRoute({
   component: RootComponent,
 })
 
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === "production"
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        }))
+      );
+
 function RootComponent() {
   return (
     <React.Fragment>
+      <React.Suspense>
+        <TanStackRouterDevtools />
+      </React.Suspense>
       <Outlet />
     </React.Fragment>
-  )
+  );
 }
