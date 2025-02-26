ed17d5e097b8d40a52f76f84e6106c16ada0658e
yasmineMei <meiteyasmine90@gmail.com>
2025-02-26 13:43:34 +0100

    other page

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -11,14 +11,27 @@
 // Import Routes
 
 import { Route as rootRoute } from './routes/__root'
+import { Route as HomeImport } from './routes/_home'
 import { Route as IndexImport } from './routes/index'
+import { Route as HomeDashboardImport } from './routes/_home/dashboard'
 
 // Create/Update Routes
+
+const HomeRoute = HomeImport.update({
+  id: '/_home',
+  getParentRoute: () => rootRoute,
+} as any)
 
 const IndexRoute = IndexImport.update({
   id: '/',
   path: '/',
   getParentRoute: () => rootRoute,
+} as any)
+
+const HomeDashboardRoute = HomeDashboardImport.update({
+  id: '/dashboard',
+  path: '/dashboard',
+  getParentRoute: () => HomeRoute,
 } as any)
 
 // Populate the FileRoutesByPath interface
@@ -32,39 +45,71 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof IndexImport
       parentRoute: typeof rootRoute
     }
+    '/_home': {
+      id: '/_home'
+      path: ''
+      fullPath: ''
+      preLoaderRoute: typeof HomeImport
+      parentRoute: typeof rootRoute
+    }
+    '/_home/dashboard': {
+      id: '/_home/dashboard'
+      path: '/dashboard'
+      fullPath: '/dashboard'
+      preLoaderRoute: typeof HomeDashboardImport
+      parentRoute: typeof HomeImport
+    }
   }
 }
 
 // Create and export the route tree
 
+interface HomeRouteChildren {
+  HomeDashboardRoute: typeof HomeDashboardRoute
+}
+
+const HomeRouteChildren: HomeRouteChildren = {
+  HomeDashboardRoute: HomeDashboardRoute,
+}
+
+const HomeRouteWithChildren = HomeRoute._addFileChildren(HomeRouteChildren)
+
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
+  '': typeof HomeRouteWithChildren
+  '/dashboard': typeof HomeDashboardRoute
 }
 
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
+  '': typeof HomeRouteWithChildren
+  '/dashboard': typeof HomeDashboardRoute
 }
 
 export interface FileRoutesById {
   __root__: typeof rootRoute
   '/': typeof IndexRoute
+  '/_home': typeof HomeRouteWithChildren
+  '/_home/dashboard': typeof HomeDashboardRoute
 }
 
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/'
+  fullPaths: '/' | '' | '/dashboard'
   fileRoutesByTo: FileRoutesByTo
-  to: '/'
-  id: '__root__' | '/'
+  to: '/' | '' | '/dashboard'
+  id: '__root__' | '/' | '/_home' | '/_home/dashboard'
   fileRoutesById: FileRoutesById
 }
 
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
+  HomeRoute: typeof HomeRouteWithChildren
 }
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
+  HomeRoute: HomeRouteWithChildren,
 }
 
 export const routeTree = rootRoute
@@ -77,11 +122,22 @@ export const routeTree = rootRoute
     "__root__": {
       "filePath": "__root.tsx",
       "children": [
-        "/"
+        "/",
+        "/_home"
       ]
     },
     "/": {
       "filePath": "index.tsx"
+    },
+    "/_home": {
+      "filePath": "_home.tsx",
+      "children": [
+        "/_home/dashboard"
+      ]
+    },
+    "/_home/dashboard": {
+      "filePath": "_home/dashboard.tsx",
+      "parent": "/_home"
     }
   }
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,15 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/')({
+import { createFileRoute } from "@tanstack/react-router";
+import { LoginForm } from "@/components/login-form";
+export const Route = createFileRoute("/")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/"!</div>
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
+    </div>
+  );
 }
